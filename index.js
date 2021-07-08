6ee35672ec79bc1f843809d86e62057b5e365bbf
Grimille <mickael.dhainaut@gmail.com>
2021-07-08 21:32:14 +0200

    🗃  Added read db

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const database = readDatabase();
 
 dotenv.config();
 
@@ -14,18 +15,23 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/plants', function(req, res) {
-    return res.json({message: "Plants"})
-});
-
-app.get('/api/user/plants/tasks', function (req, res) {
-    return res.json({message: "Tasks"})
+    return res.json(database.plants);
 });
 
 app.post('/api/plants/search', function(req, res) {
     return res.json({message: "Search"})
 });
 
+app.get('/api/user/plants/tasks', function (req, res) {
+    return res.json(database.tasks);
+});
+
 
 app.listen(3009, function() {
     console.log("Listening on port 3009");
 });
+
+function readDatabase() {
+    const raw = fs.readFileSync(path.resolve(__dirname, 'database.json'));
+    return JSON.parse(raw);
+}
